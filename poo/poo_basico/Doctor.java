e2844ef6c59af6b4dbf2e40ca9a1d4e0dc2c33c6
Brianmax <george.maxi@ucsp.edu.pe>
2023-08-07 16:15:44 -0500

    first commit

---
+++ poo/poo_basico/Doctor.java
@@ -0,0 +1,13 @@
+public class Doctor {
+    //Atributos
+    String nombre;
+    String ciudad;
+    int edad;
+
+    //metodos
+    void mostrarInfo(){
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Ciudad: " + ciudad);
+        System.out.println("Edad: " + edad);
+    }
+}
